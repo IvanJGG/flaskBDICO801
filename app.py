e78a639e5deb4069466491a2fc65aa692bb63201
IvanAdmin <ivangalladoguevara@gmail.com>
2026-02-20 09:49:41 -0600

    20/02/2026

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import forms
 
 app = Flask(__name__)
 
@@ -7,10 +8,24 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
-@app.route("/")
-@app.route("/usuario")
-def usuario():
-	return render_template("usuario.html")
+@app.route("/usuarios", methods=["GET", "POST"])
+def usuarios():
+	mat=0
+	nom=''
+	apa=''
+	ama=''
+	edad=0
+	email=''
+	usuario_clas=forms.UserForm(request.form)
+	if request.method == "POST" and usuario_clas.validate():
+		mat=usuario_clas.mat.data
+		nom=usuario_clas.nom.data
+		apa=usuario_clas.apa.data
+		ama=usuario_clas.ama.data
+		edad=usuario_clas.edad.data
+		email=usuario_clas.email.data
+
+	return render_template("usuarios.html",form=usuario_clas, mat=mat, nom=nom, apa=apa, ama=ama, edad=edad, email=email)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from wtforms import Form
-from wtforms import StringField, PasswordField, IntegerField, EmailField
+from wtforms import StringField, IntegerField, EmailField
 from wtforms.validators import DataRequired, Length, Email
 
 class UserForm(Form):
-    nombre = StringField('Nombre', validators=[DataRequired(), Length(min=2, max=50)])
-    apaterno = StringField('Apellido Paterno', validators=[DataRequired(), Length(min=2, max=50)])
-    amaterno = StringField('Apellido Materno', validators=[DataRequired(), Length(min=2, max=50)])
-    edad = IntegerField('Edad', validators=[DataRequired()])
-    correo = EmailField('Correo Electrónico', validators=[DataRequired(), Email()])
+    mat = IntegerField('Matrícula')
+    nom = StringField('Nombre', validators=[DataRequired(), Length(min=2, max=50)])
+    apa = StringField('Apellido Paterno', validators=[DataRequired(), Length(min=2, max=50)])
+    ama = StringField('Apellido Materno', validators=[DataRequired(), Length(min=2, max=50)])
+    edad = IntegerField('Edad')
+    email = EmailField('Correo Electrónico', validators=[DataRequired(), Email()])
