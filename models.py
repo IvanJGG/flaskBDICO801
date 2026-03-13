a16fb9300cd84a5fd9f4ec99683e6962edc24e4a
IvanAdmin <ivangalladoguevara@gmail.com>
2026-03-13 09:42:54 -0600

    13/03/2026 Migracion

--- models.py
+++ models.py
@@ -6,18 +6,18 @@ db = SQLAlchemy()
 class Alumnos(db.Model):
     __tablename__ = 'alumnos'
     id = db.Column(db.Integer, primary_key=True)
-    nom = db.Column(db.String(50), nullable=False)
-    apa = db.Column(db.String(50), nullable=False)
-    ama = db.Column(db.String(50), nullable=False)
+    nom = db.Column(db.String(250), nullable=False)
+    apa = db.Column(db.String(250), nullable=False)
+    ama = db.Column(db.String(250), nullable=False)
     edad = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
 
 class Maestros(db.Model):
     __tablename__ = 'maestros'
     id = db.Column(db.Integer, primary_key=True)
-    nom = db.Column(db.String(50), nullable=False)
-    apa = db.Column(db.String(50), nullable=False)
-    ama = db.Column(db.String(50), nullable=False)
+    nom = db.Column(db.String(250), nullable=False)
+    apa = db.Column(db.String(250), nullable=False)
+    ama = db.Column(db.String(250), nullable=False)
     edad = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
 
